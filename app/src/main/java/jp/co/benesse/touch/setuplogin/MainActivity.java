5e43b041c86f94cf1155b45c3346b7aff575a91e
Syuugo <pub@s1204.me>
2022-09-14 18:14:08 +0900

    v1.2: コードの整形

--- app/src/main/java/jp/co/benesse/touch/setuplogin/MainActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/MainActivity.java
@@ -1,16 +1,15 @@
 package jp.co.benesse.touch.setuplogin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.content.Intent;
 import android.provider.Settings;
 import android.provider.Settings.System;
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
         setContentView(R.layout.main);
         System.putInt(getContentResolver(), "dcha_state", 3);
         System.putInt(getContentResolver(), "hide_navigation_bar", 0);
