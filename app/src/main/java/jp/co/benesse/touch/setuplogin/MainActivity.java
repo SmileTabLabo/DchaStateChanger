9abfcadc63ce1f43ba172bb035985ba54763af86
Syuugo <pub@s1204.me>
2022-11-20 14:40:59 +0900

    Keep original and fix

--- app/src/main/java/jp/co/benesse/touch/setuplogin/MainActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/MainActivity.java
@@ -1,33 +1,18 @@
 package jp.co.benesse.touch.setuplogin;
 
-import android.app.*;
-import android.os.*;
-import android.provider.Settings.System;
-import android.content.Intent;
-import android.net.Uri;
-public class MainActivity extends Activity 
-{
+import android.app.Activity;
+import android.os.Bundle;
+import static android.provider.Settings.System.putInt;
+
+public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {   //文字表示
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 文字表示
         setContentView(R.layout.main);
-		//dcha 3 変更
-		System.putInt(getContentResolver(), "dcha_state", 3);
-		System.putInt(getContentResolver(), "hide_navigation_bar", 0);
-		//開発者向けオプションを開く
-		Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
-		startActivity(intent);
-		
-		//3秒後にdcha 0に変更
-		new Handler().postDelayed(new Runnable() {
-				@Override
-				public void run() {
-					//dcha 0 変更
-					System.putInt(getContentResolver(), "dcha_state", 0);
-					
-				}
-			}, 3000);
-		
+        // DchaStateを 3 に変更
+        putInt(getContentResolver(), "dcha_state", 3);
+        // ナビゲーションバーを表示
+        putInt(getContentResolver(), "hide_navigation_bar", 0);
     }
 }
