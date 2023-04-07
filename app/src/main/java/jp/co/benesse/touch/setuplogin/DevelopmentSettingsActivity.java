5d41ccff8108d3b77e8658b9e2aadce949f0c39b
Syuugo <pub@s1204.me>
2023-04-07 14:00:04 +0900

    BypassDchaLock を統合

---
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/DevelopmentSettingsActivity.java
@@ -0,0 +1,40 @@
+package jp.co.benesse.touch.setuplogin;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import static android.net.Uri.parse;
+import static android.provider.Settings.Global.getInt;
+import static android.provider.Settings.SettingNotFoundException;
+import static android.provider.Settings.System.canWrite;
+import static android.provider.Settings.System.putInt;
+
+public class DevelopmentSettingsActivity extends Activity {
+    @SuppressLint("WrongConstant")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finishAndRemoveTask();
+        if (!canWrite(getApplicationContext())) {
+            startActivity(new Intent("android.settings.action.MANAGE_WRITE_SETTINGS", parse("package:jp.co.benesse.touch.setuplogin")).setFlags(268435456));
+        } else {
+            putInt(getContentResolver(), "dcha_state", 3);
+            startActivity(new Intent("android.settings.APPLICATION_DEVELOPMENT_SETTINGS"));
+            try {
+                if ((getInt(getContentResolver(), "development_settings_enabled") == 1) && (getInt(getContentResolver(), "adb_enabled") == 1)) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            putInt(DevelopmentSettingsActivity.this.getContentResolver(), "dcha_state", 0);
+                        }
+                    }, 1000);
+                }
+            } catch (SettingNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -1,12 +1,14 @@
 package jp.co.benesse.touch.setuplogin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.File;
 
+import static android.net.Uri.parse;
 import static android.provider.Settings.System.putInt;
 
 public class LoginSettingActivity extends Activity {
@@ -17,6 +19,9 @@ public class LoginSettingActivity extends Activity {
         if (new File("/factory/count_dcha_completed").exists()) {
             // DchaStateを３に変更
             putInt(getContentResolver(), "dcha_state", 3);
+        } else {
+            // 必要無い場合はアンインストールを要求
+            startActivity(new Intent("android.intent.action.DELETE").setData(parse("package:jp.co.benesse.touch.setuplogin")));
         }
         // ナビゲーションバーを表示
         putInt(getContentResolver(), "hide_navigation_bar", 0);
