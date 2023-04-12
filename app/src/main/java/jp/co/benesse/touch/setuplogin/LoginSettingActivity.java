1b1dbc7d37a4387072ef0f0dc753d0f2f2ee22fd
Syuugo <pub@s1204.me>
2023-04-12 23:23:31 +0900

    v1.4.3

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -15,22 +15,26 @@ public class LoginSettingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 文字表示のセットアップ
+        String msg;
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setGravity(17);
+        TextView textView = new TextView(this);
+        linearLayout.addView(textView, new LinearLayout.LayoutParams(-2, -2));
         // DchaStateが３に成った形跡が有る場合
         if (new File("/factory/count_dcha_completed").exists()) {
             // DchaStateを３に変更
             putInt(getContentResolver(), "dcha_state", 3);
+            msg = "DchaState を 3 に設定しました";
         } else {
             // 必要無い場合はアンインストールを要求
-            startActivity(new Intent("android.intent.action.DELETE").setData(parse("package:jp.co.benesse.touch.setuplogin")));
+            startActivity(new Intent(Intent.ACTION_DELETE).setData(parse("package:jp.co.benesse.touch.setuplogin")));
+            msg = "このアプリをアンインストールしてください";
         }
         // ナビゲーションバーを表示
         putInt(getContentResolver(), "hide_navigation_bar", 0);
         // 文字表示
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setGravity(17);
-        TextView textView = new TextView(this);
-        textView.setText("ホームボタンを押して､ 設定アプリから開発者向けオプションを有効にして下さい｡");
-        linearLayout.addView(textView, new LinearLayout.LayoutParams(-2, -2));
+        textView.setText(msg);
         setContentView(linearLayout);
     }
 }
