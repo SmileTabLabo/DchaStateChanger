176b5eda2f39a353866b7a62d76c3f5a75cc67fd
Syuugo <pub@s1204.me>
2023-05-27 16:53:50 +0900

    整形のテスト

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -21,17 +21,18 @@ public class LoginSettingActivity extends Activity {
         linearLayout.setGravity(17);
         TextView textView = new TextView(this);
         linearLayout.addView(textView, new LinearLayout.LayoutParams(-2, -2));
+        textView.setTextSize(2, 50);
         // DchaStateが３に成った形跡が有る場合
         if (COUNT_DCHA_COMPLETED_FILE.exists()) {
             // DchaStateを３に変更
             setDchaState(3);
             msg = "DchaState を 3 に設定しました";
+            // アクティビティを有効化
+            getPackageManager().setComponentEnabledSetting(new ComponentName(this, DchaStateChanger.class), 1, 1);
         } else {
             // 必要無い場合はアンインストールを要求
-            startActivity(new Intent(Intent.ACTION_DELETE).setData(parse("package:jp.co.benesse.touch.setuplogin")));
+            startActivity(new Intent("android.intent.action.DELETE").setData(parse("package:jp.co.benesse.touch.setuplogin")));
             msg = "このアプリをアンインストールしてください";
-            // アクティビティを無効化
-            getPackageManager().setComponentEnabledSetting(new ComponentName(this, DchaStateChanger.class), 2, 1);
         }
         // ナビゲーションバーを表示
         putInt(getContentResolver(), "hide_navigation_bar", 0);
