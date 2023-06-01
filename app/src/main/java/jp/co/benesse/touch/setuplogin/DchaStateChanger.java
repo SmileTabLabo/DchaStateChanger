34b88676f5562c98e7cb4ec638e425cf5a94130e
s1204IT <pub@s1204.me>
2023-06-01 12:49:12 +0900

    `DchaStateChanger` を整形

--- app/src/main/java/jp/co/benesse/touch/setuplogin/DchaStateChanger.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/DchaStateChanger.java
@@ -2,18 +2,16 @@ package jp.co.benesse.touch.setuplogin;
 
 import android.app.Activity;
 import android.os.Bundle;
+
 import static android.os.BenesseExtension.*;
 
 public class DchaStateChanger extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         finishAndRemoveTask();
-        // DchaState が 3 の時に 0 に設定する
         if (getDchaState() == 3) {
             setDchaState(0);
         } else {
-            // 3以外の時は 3 に設定する
             setDchaState(3);
         }
     }
