7e890b0acec66b119fd221b759af9ceb1b08193f
Syuugo <pub@s1204.me>
2023-06-22 18:29:58 +0900

    DchaService を使用

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -3,15 +3,19 @@ package jp.co.benesse.touch.setuplogin;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import jp.co.benesse.dcha.dchaservice.IDchaService;
 import static android.os.BenesseExtension.*;
 
 public class LoginSettingActivity extends Activity {
+    IDchaService mDchaService;
     public void onCreate(Bundle savedInstanceState) {
         String msg;
         super.onCreate(savedInstanceState);
@@ -25,11 +29,24 @@ public class LoginSettingActivity extends Activity {
             getPackageManager().setComponentEnabledSetting(new ComponentName(this, DchaStateChanger.class), 1, 1);
             msg = "DchaState を 3 に設定しました";
         } else {
-            Intent intent = new Intent("android.intent.action.DELETE");
-            startActivity(intent.setData(Uri.parse("package:" + getPackageName())));
+            startActivity(new Intent("android.intent.action.DELETE").setData(Uri.parse("package:" + getPackageName())));
             msg = "このアプリをアンインストールしてください";
         }
-        Settings.System.putInt(getContentResolver(), "hide_navigation_bar", 0);
+        bindService(new Intent("jp.co.benesse.dcha.dchaservice.DchaService").setPackage("jp.co.benesse.dcha.dchaservice"), new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                mDchaService = IDchaService.Stub.asInterface(iBinder);
+                try {
+                    mDchaService.hideNavigationBar(false);
+                } catch (RemoteException ignored) {
+                }
+                unbindService(this);
+            }
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                unbindService(this);
+            }
+        }, 1);
         textView.setText(msg);
         setContentView(linearLayout);
     }
