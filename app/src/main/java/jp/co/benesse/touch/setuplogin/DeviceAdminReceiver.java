eb312b8fb634418c07cb0c47c732e1f47099afb7
Syuugo <pub@s1204.me>
2023-08-10 17:00:42 +0900

    予定機能を削除

--- app/src/main/java/jp/co/benesse/touch/setuplogin/DeviceAdminReceiver.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/DeviceAdminReceiver.java
@@ -1,21 +1,4 @@
 package jp.co.benesse.touch.setuplogin;
 
-import android.app.admin.DevicePolicyManager;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-
 public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
-    /* 有効時にパスワードを削除したい
-    public void onEnabled(Context context, Intent intent) {
-        super.onEnabled(context, intent);
-        ((DevicePolicyManager) context.getApplicationContext().getSystemService(Context.DEVICE_POLICY_SERVICE)).resetPassword("", 0);
-    }
-    */
-    /* 無効時にパッケージをアンインストールしたい
-    public void onDisabled(Context context, Intent intent) {
-        super.onDisabled(context, intent);
-        context.startActivity(new Intent(Intent.ACTION_DELETE).setData(Uri.parse("package:" + context.getPackageName())));
-    }
-    */
 }
